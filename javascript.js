ca00ea02cf6d271b6c591be047a9effe04dcd467
Cristianmarzo1982 <126625009+Cristianmarzo1982@users.noreply.github.com>
2023-02-28 20:54:11 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("boton").addEventListener("click", abrirNuevaVentana);
+
+function abrirNuevaVentana()
+ {
+  window.open("/Users/cristianalvarez/Downloads/Argentina Programa 2022/Desarrollo Frontend Enero 2023/ProyectoCVAlvarezCristian/proyecto/cvAlvarezCristian.html");
+ }
